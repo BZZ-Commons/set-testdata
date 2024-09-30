21785c7575f9ec71816ec3071ec07c9654e411bd
Marcel Suter <marcel.suter@bzz.ch>
2024-09-30 08:46:51 +0200

    new setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 import hashlib
 import os
+from urllib.request import urlopen
 
 from github import Github, UnknownObjectException  # needs PyGitHub
 
-
 GITHUB_TOKEN = os.environ['GH_TOKEN']
 TARGET_REPO = os.environ['TARGET_REPO']
-TEMPLATE_REPO = os.environ['TEMPLATE_REPO']
+TEMPLATE_PATH = os.environ['TEMPLATE_PATH']
 FILES = os.environ['FILES']
 USERNAME = os.environ['USERNAME']
+
 
 def main():
     hash = create_hash()
@@ -17,17 +18,33 @@ def main():
 
     target_repo = token.get_repo(TARGET_REPO)
     for file in files:
-        contents = f'""" Provides the class "{file}"\t{hash} """\nclass {file}:\n\tpass'
-        target_repo.create_file(
-            path=f'{file}.py',
-            message='create class',
-            content=contents
+        try:
+            target_repo.get_contents(f'{file}.py')
+            print (f'{file}.py already exists')
+        except UnknownObjectException as e:
+
+            contents = read_template(file, hash)
+            target_repo.create_file(
+                path=f'{file}.py',
+                message='create class',
+                content=contents
         )
+
+
+def read_template(filename, hash):
+    response = urlopen(f'{TEMPLATE_PATH}{filename}.csv')
+    template = ''
+    for line in response.readlines():
+        template += line.decode('utf-8')
+
+    return template.replace('{{HASH}}', hash)
+
 
 def create_hash():
     hash = hashlib.sha256(USERNAME.encode()).hexdigest()
     print(hash)
     return hash[:8]
+
 
 if __name__ == '__main__':
     main()
