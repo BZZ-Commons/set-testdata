1383631598fc6b9457c476ad2903ee316c41eda8
Marcel Suter <marcel.suter@bzz.ch>
2024-09-30 07:46:08 +0200

    new setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+import hashlib
+import os
+
+from github import Github, UnknownObjectException  # needs PyGitHub
+
+GITHUB_TOKEN = os.environ['GH_TOKEN']
+TARGET_REPO = os.environ['TARGET_REPO']
+TEMPLATE_REPO = os.environ['TEMPLATE_REPO']
+FILES = os.environ['FILES']
+OWNER = os.environ['OWNER']
+
+def main():
+    hash = create_hash()
+    files = FILES.split(',')
+    token = Github(GITHUB_TOKEN)
+    target_repo = token.get_repo(TARGET_REPO)
+    for file in files:
+        contents = f'""" Provides the class "{file}"\t{hash} """\nclass {file}:\n\tpass'
+        target_repo.create_file(
+            path=f'{file}.py',
+            message='create class',
+            content=contents
+        )
+
+def create_hash():
+    hash = hashlib.sha256(OWNER.encode()).hexdigest()
+    print(hash)
+    return hash[:8]
+
+if __name__ == '__main__':
+    main()
